78da875110991af390d7659a5791389fc8cb0955
KacperBieganski <KacperBieganski@users.noreply.github.com>
2025-08-16 14:37:05 +0000

    deploy: 9e0e3952778e2dc2193fdf666616db3f4aed6365

---
+++ assets/images.tsx
@@ -0,0 +1,55 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="images" tilewidth="320" tileheight="256" tilecount="17" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image source="objects/bushe1.png" width="128" height="128"/>
+ </tile>
+ <tile id="1">
+  <image source="objects/bushe2.png" width="128" height="128"/>
+ </tile>
+ <tile id="2">
+  <image source="objects/bushe3.png" width="128" height="128"/>
+ </tile>
+ <tile id="3">
+  <image source="objects/bushe4.png" width="128" height="128"/>
+ </tile>
+ <tile id="4">
+  <image source="objects/redCastle.png" width="320" height="256"/>
+ </tile>
+ <tile id="5">
+  <image source="objects/redHouse1.png" width="128" height="192"/>
+ </tile>
+ <tile id="6">
+  <image source="objects/redHouse2.png" width="128" height="192"/>
+ </tile>
+ <tile id="7">
+  <image source="objects/redHouse3.png" width="128" height="192"/>
+ </tile>
+ <tile id="8">
+  <image source="objects/redTower.png" width="128" height="256"/>
+ </tile>
+ <tile id="9">
+  <image source="objects/Rock1.png" width="64" height="64"/>
+ </tile>
+ <tile id="10">
+  <image source="objects/Rock2.png" width="64" height="64"/>
+ </tile>
+ <tile id="11">
+  <image source="objects/Rock3.png" width="64" height="64"/>
+ </tile>
+ <tile id="12">
+  <image source="objects/Rock4.png" width="64" height="64"/>
+ </tile>
+ <tile id="13">
+  <image source="objects/tree1.png" width="192" height="256"/>
+ </tile>
+ <tile id="14">
+  <image source="objects/tree2.png" width="192" height="256"/>
+ </tile>
+ <tile id="15">
+  <image source="objects/tree3.png" width="192" height="192"/>
+ </tile>
+ <tile id="16">
+  <image source="objects/tree4.png" width="192" height="192"/>
+ </tile>
+</tileset>
